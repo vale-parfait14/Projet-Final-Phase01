a52dbedcc42c333390322ef6dfd7a2692da73181
oz ovale <ozovale15@gmail.com>
2024-08-28 02:43:50 +0000

    first commit

---
+++ frontend/src/components/signin/Signin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Signin.css"
+const Signin = () => {
+  return (
+    <div className='signin'>Signin</div>
+  )
+}
+
+export default Signin;
---
+++ frontend/src/components/signup/Signup.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Signup.css"
+const Signup = () => {
+  return (
+    <div className='signup'>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-8 column">SIGNUP</div>
+                <div className="col-lg-4  column d-lex justify-content-center align-items-center">
+                    <h1 className='text-center sign-up-heading' >
+                        Sign <br /> Up
+                    </h1>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Signup;
